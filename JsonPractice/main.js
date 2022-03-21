86c98f0cffad5bc47c31434fb5526c7abef2d763
Johan <johanvazquez2704@gmail.com>
2022-03-20 22:22:55 -0500

    Second commit

--- JsonPractice/main.js
+++ JsonPractice/main.js
@@ -2,7 +2,8 @@ async function ObtainData (){
     const response = await fetch('http://127.0.0.1:5500/JsonPractice/data.json');
     const json = await response.json();
     //console.log(JSON.stringify(json));
-    console.log(json.Hobbies)
+    let Name = (json.Experience)
+    console.log(Name)
 }
 
 ObtainData();
